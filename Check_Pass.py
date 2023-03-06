5b1e4f733190662b35b5014d7e06de947a256225
JFE-hash <egorikmenya@gmail.com>
2023-03-07 00:52:40 +0300

    Сделал

--- Check_Pass.py
+++ Check_Pass.py
@@ -1,1 +1,8 @@
-print('2')
+print('Введите пароль: ')
+pass_check1 = input()
+print('Повторите пароль: ')
+pass_check2 = input()
+if pass_check1 == pass_check2:
+    print('Пароль принят')
+else:
+    print('Пароль не принят')
